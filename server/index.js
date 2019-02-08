cac2d5f1723fdb4360a29fdf30dcea4103431311
Prateek Thapa <prateekthapa26@gmail.com>
2019-02-08 20:27:01 +0545

    basic working of graphql

--- server/index.js
+++ server/index.js
@@ -1,14 +1,39 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+const links = [
+  {
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'FullStack tutorial for Graphql',
+  },
+  {
+    id: 'link-1',
+    url: 'lallumiya.com',
+    description: 'Hello there',
+  },
+];
+
 const typeDefs = `
-  type Query {
-    info: String!
-  }
-`;
+type Query {
+  info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
+}`;
 
 const resolvers = {
   Query: {
-    info: () => null,
+    info: () => 'This is the hackernews clone',
+    feed: () => links,
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url,
   },
 };
 
